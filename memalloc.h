a9c6d5c2063701d229b65edc62bf86c516ec9df3
ThanhPhammmm <phamvanthanh2522004@gmail.com>
2025-11-14 23:02:58 +0700

    Pending

--- memalloc.h
+++ memalloc.h
@@ -2,13 +2,28 @@
 #ifdef MEMALLOC_H
 
 #include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
 
-struct header_t{
-    size_t size;
-    unsigned is_free;
-    struct header_t* next;
-};
+typedef char ALIGN[32 ];
 
+typedef union header{
+    struct{
+        size_t size;// denoting the size of user data, depending on the architecture, usually 4 or 8 bytes
+        unsigned is_free;
+        union header* next; // pointer to the next block in the linked list, depending on the architecture, usually 4 or 8 bytes
+    } s;
+    ALIGN stub;
+} header_t;
 
+header_t *head = NULL, *tail = NULL;
+pthread_mutex_t global_malloc_lock;
+
+header_t* get_free_block(size_t size);
+void* mem_malloc(size_t size);
+void mem_free(void* block);
+void* mem_realloc(void* block, size_t size);
+void* mem_calloc(size_t num, size_t nsize);
 
 #endif
